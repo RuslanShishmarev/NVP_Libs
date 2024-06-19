f8c979f8f8da990e90db7423280ed5cb95afcaae
Ruslan Shishmarev <mr.shruslan@gmail.com>
2024-06-19 16:03:06 +0300

    new files

---
+++ NVP_Libs/NVP_Libs/ExampleRevit.cs
@@ -0,0 +1,18 @@
+﻿using NVP.API.Nodes;
+using System.Collections.Generic;
+
+namespace NVP_Libs
+{
+    [NodeInput("номер 1", typeof(double))]
+    [NodeInput("номер 2", typeof(double))]
+    public class ExampleRevit : IRevitNode
+    {
+        public NodeResult Execute(IVisualViewerData context, List<NodeResult> inputs, object commandData)
+        {
+            var num1 = (double)inputs[0].Value;
+            var num2 = (double)inputs[1].Value;
+
+            return new NodeResult(num1 + num2);
+        }
+    }
+}
